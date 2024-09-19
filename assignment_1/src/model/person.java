2281ed53776d05f7f6520130354607c7f8b88433
jinkaradhakrishna.s@northeastern.edu <jinkaradhakrishna.s@northeastern.edu>
2024-09-19 01:56:35 -0400

    assignment_1 added

---
+++ assignment_1/src/model/person.java
@@ -0,0 +1,178 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author jrswa
+ */
+public class person {
+    String firstname;
+    String lastname;
+    String email;
+    String phonenumber;
+
+    String dlnumber;
+    String dlstate;
+    String SSN;
+    String occupation;
+    String pronouns;
+    String age;
+    String nationality;
+    
+    
+    String add1;
+    String add2;
+    String city;
+    String state;
+    String zip;
+    String telephone;
+    String fax;
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhonenumber() {
+        return phonenumber;
+    }
+
+    public void setPhonenumber(String phonenumber) {
+        this.phonenumber = phonenumber;
+    }
+
+    public String getDlnumber() {
+        return dlnumber;
+    }
+
+    public void setDlnumber(String dlnumber) {
+        this.dlnumber = dlnumber;
+    }
+
+    public String getDlstate() {
+        return dlstate;
+    }
+
+    public void setDlstate(String dlstate) {
+        this.dlstate = dlstate;
+    }
+
+    public String getSSN() {
+        return SSN;
+    }
+
+    public void setSSN(String SSN) {
+        this.SSN = SSN;
+    }
+
+    public String getOccupation() {
+        return occupation;
+    }
+
+    public void setOccupation(String occupation) {
+        this.occupation = occupation;
+    }
+
+    public String getPronouns() {
+        return pronouns;
+    }
+
+    public void setPronouns(String pronouns) {
+        this.pronouns = pronouns;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public String getAdd1() {
+        return add1;
+    }
+
+    public void setAdd1(String add1) {
+        this.add1 = add1;
+    }
+
+    public String getAdd2() {
+        return add2;
+    }
+
+    public void setAdd2(String add2) {
+        this.add2 = add2;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getFax() {
+        return fax;
+    }
+
+    public void setFax(String fax) {
+        this.fax = fax;
+    }
+}
+    
